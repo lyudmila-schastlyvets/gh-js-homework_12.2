4b94f51bfc697260c92895bcfe810bcd55456a32
liudmyla_schastlyvets <lyudmila.zoria@n3wnormal.com>
2018-01-13 14:47:18 +0200

    factorial function

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ isPrime(17)                         // true
 isPrime(10000000000000)             // false
 
 // 2 factorial - Returns a number that is the factorial of the given number.
+function factorial (num) {
+    if (num <= 0) {
+        return 1
+    } else {
+        return (num * factorial(num - 1))
+    }
+}
+
 factorial(0)                        // 1
 factorial(1)                        // 1
 factorial(6)                        // 720
